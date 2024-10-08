e79b8a6bd7a2dbc0c58e3804b2c770fe50eb6cab
engeneee <zhuldyzai.rashid@jihc.kz>
2024-10-08 20:58:20 +0500

    feat: task list with StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(ToDoApp());
-}
-
 class ToDoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,15 +10,56 @@ class ToDoApp extends StatelessWidget {
   }
 }
 
-class ToDoListScreen extends StatelessWidget {
+class ToDoListScreen extends StatefulWidget {
+  @override
+  _ToDoListScreenState createState() => _ToDoListScreenState();
+}
+
+class _ToDoListScreenState extends State<ToDoListScreen> {
+  List<String> tasks = [];
+
+  _addTask(String task) {
+    setState(() {
+      tasks.add(task);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    TextEditingController taskController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Список задач'),
       ),
-      body: Center(
-        child: Text('Добавьте ваши задачи'),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              controller: taskController,
+              decoration: InputDecoration(
+                labelText: 'Добавить новую задачу',
+              ),
+              onSubmitted: (value) {
+                if (value.isNotEmpty) {
+                  _addTask(value);
+                  taskController.clear();
+                }
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(tasks[index]),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
